a9d41a00ccac7a9aeff66fe443f1ebf211ffb37f
Thoriq Al Kautsar <40397338+thorryuk@users.noreply.github.com>
2026-01-23 16:18:39 +0700

    fix load env

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
+import os
 from app import create_app
+from dotenv import load_dotenv
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+load_dotenv(BASE_DIR / '.env', override=True)
+dotenv_path = BASE_DIR / "environment" / f".env.{os.getenv('APP_ENV')}"
+load_dotenv(dotenv_path=dotenv_path, override=True)
 
 app = create_app()
 
